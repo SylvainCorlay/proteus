ed4b06b5256d3131a375ec19016bfd322a6da194
Manuel Quezada de Luna <manuel.quezada.dl@gmail.com>
2017-08-06 14:21:31 -0500

    Change to use reflecting BCs on dam over bumps

--- proteus/tests/my_SWEs_invariant_domain/dam_over_bumps/sw_hump_2d_p.py
+++ proteus/tests/my_SWEs_invariant_domain/dam_over_bumps/sw_hump_2d_p.py
@@ -8,8 +8,8 @@ import math
 
 nd=2
 
-T=10.0#2*math.pi/omega
-nDTout=100
+T=30.0#2*math.pi/omega
+nDTout=300
 
 L=(75.0,30.0)
 g = 9.81
@@ -19,8 +19,9 @@ mannings=0.02
 domain = RectangularDomain(L=L)
 
 #This is relevant just when use_second_order_NonFlatB_with_EV_stabilization=True
-cE=1
+cE=2
 LUMPED_MASS_MATRIX=0
+reflecting_BCs=1
 
 bt = domain.boundaryTags
 bt['front'] = bt['bottom']
@@ -70,16 +71,22 @@ def getDBC_h(x,flag):
 
 #note, these are the same for hu and hv so we can cheat and use  this p-file for SW2DCV and SW2D
 def getDBC_u(x,flag):
-   if (x[0] in [0.0,L[0]]) or flag in [bt['left'],bt['right']]:
-       return lambda x,t: 0.0
-   else:
-       return None
+    return None
+    #if (x[0] in [0.0,L[0]]) or flag in [bt['left'],bt['right']]:
+    #if (x[0] in [L[0]]) or flag in [bt['right']]:
+    #if (x[0] in [0.0]) or flag in [bt['left']]:
+    #    return lambda x,t: 0.0
+    #else:
+    #    return None
 
 def getDBC_v(x,flag):
-   if x[1] in [0.0,L[1]] or flag in [bt['front'],bt['back']]:
-       return lambda x,t: 0.0
-   else:
-       return None
+    return None
+    #if x[1] in [0.0,L[1]] or flag in [bt['front'],bt['back']]:
+    #if (x[0] in [L[0]]) or flag in [bt['right']]:
+    #if (x[0] in [0.0]) or flag in [bt['left']]:
+    #    return lambda x,t: 0.0
+    #else:
+    #    return None
 
 dirichletConditions = {0:getDBC_h,
                        1:getDBC_u,
@@ -90,34 +97,40 @@ fluxBoundaryConditions = {0:'outFlow',
                           2:'outFlow'}
 
 def getAFBC_h(x,flag):
-    return lambda x,t: 0.0
+    return None
+#    return lambda x,t: 0.0
 
 def getAFBC_u(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
+    return None
+#    if flag == 0:
+#        return lambda x,t: 0.0
+#    else:
+#        return None
+
 def getAFBC_v(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
+    return None
+#    if flag == 0:
+#        return lambda x,t: 0.0
+#    else:
+#        return None
 
 advectiveFluxBoundaryConditions =  {0:getAFBC_h,
                                     1:getAFBC_u,
                                     2:getAFBC_v}
 
 def getDFBC_u(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
+    return None
+#    if flag == 0:
+#        return lambda x,t: 0.0
+#    else:
+#        return None
 
 def getDFBC_v(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
+    return None
+#    if flag == 0:
+#        return lambda x,t: 0.0
+#    else:
+#        return None
 
 diffusiveFluxBoundaryConditions = {0:{},
                                    1:{1:getDFBC_u},
